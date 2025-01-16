709d0b811f505f0132b3f8bd663a17389a8af5a7
huytq.atomi <huytq@atomi.com.vn>
2025-01-16 16:29:30 +0700

    icon - gradient

--- lib/screens/voting/widget/submit_button.dart
+++ lib/screens/voting/widget/submit_button.dart
@@ -1,8 +1,10 @@
+import 'package:atomi_yep/constant/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../cubits/vote/vote_cubit.dart';
 import '../../../cubits/vote/vote_state.dart';
+
 class SubmitButton extends StatelessWidget {
   final String eventId;
 
@@ -12,35 +14,39 @@ class SubmitButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<VoteCubit, VoteState>(
       buildWhen: (previous, current) =>
-      previous.canSubmit != current.canSubmit ||
+          previous.canSubmit != current.canSubmit ||
           previous.status != current.status,
       builder: (context, state) {
         final buttonColor = state.canSubmit ? Color(0xFF25449C) : Colors.grey;
         final textColor = state.canSubmit ? Colors.white : Colors.grey;
 
-        return Padding(
-          padding: EdgeInsets.all(16),
-          child: ElevatedButton(
-            onPressed: state.canSubmit
-                ? () => context.read<VoteCubit>().submitVote(eventId)
-                : null,
-            style: ElevatedButton.styleFrom(
-              backgroundColor: buttonColor,
-            ),
-            child: Container(
-              width: double.infinity,
-              height: 48,
-              child: Center(
-                child: state.status == VoteStatus.submitting
-                    ? CircularProgressIndicator(color: Colors.white)
-                    : Text(
-                  'Bình chọn',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: textColor,
-                  ),
-                ),
-              ),
+        final gradientSubmit = state.canSubmit
+            ? GradientUtils.primaryGradient
+            : GradientUtils.offGradient;
+
+        return InkWell(
+          onTap: state.canSubmit
+              ? () => context.read<VoteCubit>().submitVote(eventId)
+              : null,
+          // style: ElevatedButton.styleFrom(
+          //   backgroundColor: buttonColor,
+          // ),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                gradient: gradientSubmit),
+            width: double.infinity,
+            height: 48,
+            child: Center(
+              child: state.status == VoteStatus.submitting
+                  ? CircularProgressIndicator(color: Colors.white)
+                  : Text(
+                      'Bình chọn',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: AppColors.white,
+                      ),
+                    ),
             ),
           ),
         );
